4e8faa52689e2a38a10978d3e36e287fde6597c8
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-30 22:07:46 +0200

    Add files via upload

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public enum DniTygodnia
+{
+    Poniedzialek,
+    Wtorek,
+    Sroda,
+    Czwartek,
+    Piatek,
+    Sobota,
+    Niedziela
+}
+
+public enum EtapyPrania
+{
+    WstepnePlukanie,
+    PranieZasadnicze,
+    Plukanie,
+    Wirowanie
+}
+
+public enum Posilki
+{
+    Sniadanie,
+    DrugieSniadanie,
+    Obiad,
+    Podwieczorek,
+    Kolacja
+}
+
+public enum BierkiSzachowe
+{
+    Pionek,
+    Wieza,
+    Skoczek,
+    Goniec,
+    Hetman,
+    Krol
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DniTygodnia dzienTygodnia = DniTygodnia.Piatek;
+        Console.WriteLine("Dzisiejszy dzień tygodnia: " + dzienTygodnia);
+
+        EtapyPrania etapPrania = EtapyPrania.PranieZasadnicze;
+        Console.WriteLine("Aktualny etap prania: " + etapPrania);
+
+        Posilki posilek = Posilki.Obiad;
+        Console.WriteLine("Dzisiejszy posiłek: " + posilek);
+
+        BierkiSzachowe bierkaSzachowa = BierkiSzachowe.Skoczek;
+        Console.WriteLine("Wybrana bierka szachowa: " + bierkaSzachowa);
+
+        Console.ReadLine();
+    }
+}
